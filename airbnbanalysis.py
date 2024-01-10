1bf62d156c53304167a35ae9162853ccedefdb4d
SUDHEER2002 <98392941+SUDHEER2002@users.noreply.github.com>
2024-01-10 19:49:47 +0530

    Update airbnbanalysis.py

--- airbnbanalysis.py
+++ airbnbanalysis.py
@@ -1,233 +1,201 @@
-import streamlit as st
-from streamlit_folium import st_folium
-import folium
+# Importing Libraries
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
+import pymongo
+import streamlit as st
 import plotly.express as px
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
+from streamlit_option_menu import option_menu
+from PIL import Image
 
-# Setting Webpage Configurations
-st.set_page_config(page_icon="🌎",page_title="Airbnb", layout="wide")
+# Setting up page configuration
+icon = Image.open("ICN.png")
+st.set_page_config(page_title= "Airbnb Data Visualization | By SudheerBv",
+                   page_icon= icon,
+                   layout= "wide",
+                   initial_sidebar_state= "expanded",
+                   menu_items={'About': """# This dashboard app is created by *SudheerBv*!
+                                        Data has been gathered from mongodb atlas"""}
+                  )
 
-st.set_option('deprecation.showPyplotGlobalUse', False)
+# Creating option menu in the side bar
+with st.sidebar:
+    selected = option_menu("Menu", ["Home","Overview","Explore"], 
+                           icons=["house","graph-up-arrow","bar-chart-line"],
+                           menu_icon= "menu-button-wide",
+                           default_index=0,
+                           styles={"nav-link": {"font-size": "20px", "text-align": "left", "margin": "-2px", "--hover-color": "#FF5A5F"},
+                                   "nav-link-selected": {"background-color": "#FF5A5F"}}
+                          )
 
-st.title(':rainbow[AirBnb Geospatial Analysis] 🌍')
+# CREATING CONNECTION WITH MONGODB ATLAS AND RETRIEVING THE DATA
+client = pymongo.MongoClient("mongodb+srv://sudheer:j1dsiFhk6hklRlV1@cluster0.parsrd3.mongodb.net/?retryWrites=true&w=majority")
+db = client.sample_airbnb
+col = db.listingsAndReviews
 
-data = pd.read_csv(r'C:\\Users\\DELL\\OneDrive - Amrita vishwa vidyapeetham\\Desktop\\New folder (3)\\airbnb_dataset.csv')
+# READING THE CLEANED DATAFRAME
+df = pd.read_csv('New_data.csv')
 
-aggregated = data.groupby(['country','city']).count()
-
-tab1,tab2 = st.tabs(['Geospatial Analysis 🌍','Exploratory Data Analysis 📈'])
-
-with tab1:
-    country_in = st.selectbox('Select a Country',options=data['country'].unique())
-
-    if country_in == 'Australia':
-        city_in = st.selectbox('Select a City',options=['Sydney'])
-
-    elif country_in == 'Brazil':
-        city_in = st.selectbox('Select a City',options=['Rio De Janeiro'])
-
-    elif country_in == 'Canada':
-        city_in = st.selectbox('Select a City',options=['Montreal'])
-
-    elif country_in == 'China':
-        city_in = st.selectbox('Select a City',options=['Hong Kong'])
-
-    elif country_in == 'Hong Kong':
-        city_in = st.selectbox('Select a City',options=['Hong Kong'])
-
-    elif country_in == 'Portugal':
-        city_in = st.selectbox('Select a City',options=['Porto','Other (International)'])
-
-    elif country_in == 'Spain':
-        city_in = st.selectbox('Select a City',options=['Barcelona'])
-
-    elif country_in == 'Turkey':
-        city_in = st.selectbox('Select a City',options=['Istanbul'])
-
-    elif country_in == 'United States':
-        city_in = st.selectbox('Select a City',options=['Kauai','Maui','New York','Oahu','The Big Island','Other (Domestic)'])
-
-    sep1,sep2 = st.columns(2)
-
-    with sep1:
-        min_price = st.text_input('Select a Minimum Price (Min value : 9)')
+# HOME PAGE
+if selected == "Home":
+    # Title Image
+    st.image("title.png")
+    col1,col2 = st.columns(2,gap= 'medium')
+    col1.markdown("## :blue[Domain] : Travel Industry, Property Management and Tourism")
+    col1.markdown("## :blue[Technologies used] : Python, Pandas, Plotly, Streamlit, MongoDB")
+    col1.markdown("## :blue[Overview] : To analyze Airbnb data using MongoDB Atlas, perform data cleaning and preparation, develop interactive visualizations, and create dynamic plots to gain insights into pricing variations, availability patterns, and location-based trends. ")
+    col2.markdown("#   ")
+    col2.markdown("#   ")
+    col2.image("home.jpeg")
     
-    with sep2:
-        max_price = st.text_input('Select a Maximum Price (Max value : 11,681)')
-
-    st.divider() 
-
-    if country_in and city_in and min_price and max_price:
-        try:
-            query_df = data.query(f'country == "{country_in}" and city == "{city_in}" and price>={min_price} and price<={max_price}')
-            reset_index = query_df.reset_index(drop = True)
-        
-            # Creating map using folium
-            base_latitude = reset_index.loc[0,'latitude']
-            base_longitude = reset_index.loc[0,'longitude']
-
-            base_map = folium.Map(location=[base_latitude,base_longitude], zoom_start=12) # type: ignore
-
-            for index, row in reset_index.iterrows():
-                lat,lon = row['latitude'],row['longitude']
-                id = row['id']
-                name = row['name']
-                price = row['price']
-                review = row['review_score']
-                popup_text = f"ID: {id} | Name: {name} | Price: ${price} | Rating: {review}/10"
-                folium.Marker(location=[lat, lon], popup=popup_text).add_to(base_map)
-
-            # call to render Folium map in Streamlit
-            st_data = st_folium(base_map, width=1200,height = 600)
-
-            st.divider()
-
-            st.subheader(':orange[Top Hotels Recommendations] 🏰')
-            
-            df = reset_index.sort_values(by=['price','review_score'],ascending = False)
-
-            new_df = df[['id','url','name','city','country','amenities','price','review_score','no_of_reviews']]
-
-            st.dataframe(new_df.head(),hide_index = True,width=1175,height=218)
-
-            st.divider() 
-
-            st.subheader(':orange[Top Hotels by Price and Ratings] 💸')
-
-            query_top = data.query(f'country == "{country_in}" and city == "{city_in}"')
-
-            query_1 = query_top.sort_values(by = ['price','review_score'],ascending = False)
-
-            new_df1 = query_1[['id','url','name','city','country','amenities','price','review_score','no_of_reviews']]
-
-            st.dataframe(new_df1.head(),hide_index=True,width = 1175, height = 220)
-
-            st.divider()
-
-            st.subheader(':orange[Enter the ID to know more about the Hotel and Availability] 📶')
-
-            id_input = st.text_input('Enter a ID')
-
-            if id_input:
-                new_query = reset_index.query(f'country == "{country_in}" and city == "{city_in}" and price>={min_price} and price<={max_price} and id == {id_input}')
-                new_table = new_query[['id','url','name','amenities','price','availability_30','availability_60','availability_90','availability_365','review_score','no_of_reviews']] 
-                st.dataframe(new_table,hide_index = True, width = 1175, height = 78)
-
-                st.divider()
-
-        except:
-            st.info('No results found')
-
-
-# Exploratory Data Analysis
-
-with tab2:
-
-    col1,col2 = st.columns(2)
-
-    option = st.selectbox('Exploratory Data Analysis',('Select an Analysis','Price Analysis by Country', 'Distribution of Price by Country', 'Distribution of Price using Box Plot','Scatter Plot by Price and Availability'))
-
-    st.divider() 
-    # 1. Categorical Data -- country, city, review_score
-
-    if option == 'Price Analysis by Country':
-        fig = px.histogram(data,x = 'city',animation_frame='country',color = 'country')
-        fig.update_layout(width=1200,height=500, title="Animated Histogram by City",xaxis_title="City",yaxis_title="Count")
-        st.plotly_chart(fig)
-
+# OVERVIEW PAGE
+if selected == "Overview":
+    tab1,tab2 = st.tabs(["$huge 📝 RAW DATA $", "$huge🚀 INSIGHTS $"])
+    
+    # RAW DATA TAB
+    with tab1:
+        # RAW DATA
         col1,col2 = st.columns(2)
-
-        with col1:
-        
-            country_df = data[['country','city']].value_counts()
-            new_country_df = pd.DataFrame(country_df,columns = ['Number of Hotels'])
-            st.dataframe(new_country_df,width=450,height=528)
-
-        with col2:
+        if col1.button("Click to view Raw data"):
+            col1.write(col.find_one())
+        # DATAFRAME FORMAT
+        if col2.button("Click to view Dataframe"):
+            col1.write(col.find_one())
+            col2.write(df)
        
-            grouped = data.groupby(['country','city']).agg({'price':'mean','review_score':'mean'}).sort_values(by=['price','review_score'],ascending = False)
-            grouped = grouped.round()
-            st.dataframe(grouped,width = 600,height = 528)
-        st.divider() 
-
-    # 2. Numerical Data - Price
-
-    elif option == 'Distribution of Price by Country':
-        # Distribution of Data in a Numerical Columns
-        country = st.selectbox('Select any Country',options=data['country'].unique())
-
-        if country == 'Australia':
-            city = st.selectbox('Select any City',options=['Sydney'])
-
-        elif country == 'Brazil':
-            city = st.selectbox('Select any City',options=['Rio De Janeiro'])
-
-        elif country == 'Canada':
-            city = st.selectbox('Select any City',options=['Montreal'])
-
-        elif country == 'China':
-            city = st.selectbox('Select any City',options=['Hong Kong'])
-
-        elif country == 'Hong Kong':
-            city = st.selectbox('Select any City',options=['Hong Kong'])
-
-        elif country == 'Portugal':
-            city = st.selectbox('Select any City',options=['Porto'])
-
-        elif country == 'Spain':
-            city = st.selectbox('Select any City',options=['Barcelona'])
-
-        elif country == 'Turkey':
-            city = st.selectbox('Select any City',options=['Istanbul'])
-
-        elif country == 'United States':
-            city = st.selectbox('Select any City',options=['Kauai','Maui','New York','Oahu','The Big Island'])
-
-        st.divider() 
-
-        country_price_wise = data.query(f'country == "{country}" and city == "{city}"')
-
-        plt.figure(figsize=(8,3.5))
-        fig1 = sns.distplot(country_price_wise['price'])
-        st.pyplot()
-
-        if country_price_wise["price"].skew() > 0: # type: ignore
-            st.write(f'Since the value is Positive : {country_price_wise["price"].skew()}, the curve is skewed Positively to the right side')
-        elif country_price_wise["price"].skew() < 0: # type: ignore
-            st.write(f'Since the value is Negative : {country_price_wise["price"].skew()}, the curve is skewed Negatively to the left side')
+    # INSIGHTS TAB
+    with tab2:
+        # GETTING USER INPUTS
+        country = st.sidebar.multiselect('Select a Country',sorted(df.Country.unique()),sorted(df.Country.unique()))
+        prop = st.sidebar.multiselect('Select Property_type',sorted(df.Property_type.unique()),sorted(df.Property_type.unique()))
+        room = st.sidebar.multiselect('Select Room_type',sorted(df.Room_type.unique()),sorted(df.Room_type.unique()))
+        price = st.slider('Select Price',df.Price.min(),df.Price.max(),(df.Price.min(),df.Price.max()))
         
-        st.divider() 
-
-    elif option == 'Distribution of Price using Box Plot':
-        # Box_plot
-        fig2 = px.box(data,x = 'country', y = 'price',color = 'country',width=1200, height=650)
-        st.plotly_chart(fig2)
-
-        st.divider() 
-
-    elif option == 'Scatter Plot by Price and Availability':
-        # 3. Scatter Plot (Numerical - Numerical) - (Price - Availability)
-        subplots = make_subplots(rows=4, cols=1,subplot_titles = ('Availability 30', 'Availability 60', 'Availability 90', 'Availability 365'))
-        scatter_plots1 = go.Scatter(x = data['price'],y = data['availability_30'],mode='markers', name = 'Availability 30')
-        subplots.add_trace(scatter_plots1, row=1, col=1)
+        # CONVERTING THE USER INPUT INTO QUERY
+        query = f'Country in {country} & Room_type in {room} & Property_type in {prop} & Price >= {price[0]} & Price <= {price[1]}'
         
-        scatter_plots2 = go.Scatter(x = data['price'],y = data['availability_60'],mode='markers',name = 'Availability 60')
-        subplots.add_trace(scatter_plots2, row=2, col=1)
-
-        scatter_plots3 = go.Scatter(x = data['price'],y = data['availability_90'],mode='markers',name = 'Availability 90')
-        subplots.add_trace(scatter_plots3, row=3, col=1)
-
-        scatter_plots4 = go.Scatter(x = data['price'],y = data['availability_365'],mode='markers',name = 'Availability 365')
-        subplots.add_trace(scatter_plots4, row=4, col=1)
-
-        subplots.update_layout(height=750,width = 1200) 
-        st.plotly_chart(subplots)
-
-        st.divider() 
-
-
+        # CREATING COLUMNS
+        col1,col2 = st.columns(2,gap='medium')
+        
+        with col1:
+            
+            # TOP 10 PROPERTY TYPES BAR CHART
+            df1 = df.query(query).groupby(["Property_type"]).size().reset_index(name="Listings").sort_values(by='Listings',ascending=False)[:10]
+            fig = px.bar(df1,
+                         title='Top 10 Property Types',
+                         x='Listings',
+                         y='Property_type',
+                         orientation='h',
+                         color='Property_type',
+                         color_continuous_scale=px.colors.sequential.Agsunset)
+            st.plotly_chart(fig,use_container_width=True) 
+        
+            # TOP 10 HOSTS BAR CHART
+            df2 = df.query(query).groupby(["Host_name"]).size().reset_index(name="Listings").sort_values(by='Listings',ascending=False)[:10]
+            fig = px.bar(df2,
+                         title='Top 10 Hosts with Highest number of Listings',
+                         x='Listings',
+                         y='Host_name',
+                         orientation='h',
+                         color='Host_name',
+                         color_continuous_scale=px.colors.sequential.Agsunset)
+            fig.update_layout(showlegend=False)
+            st.plotly_chart(fig,use_container_width=True)
+        
+        with col2:
+            
+            # TOTAL LISTINGS IN EACH ROOM TYPES PIE CHART
+            df1 = df.query(query).groupby(["Room_type"]).size().reset_index(name="counts")
+            fig = px.pie(df1,
+                         title='Total Listings in each Room_types',
+                         names='Room_type',
+                         values='counts',
+                         color_discrete_sequence=px.colors.sequential.Rainbow
+                        )
+            fig.update_traces(textposition='outside', textinfo='value+label')
+            st.plotly_chart(fig,use_container_width=True)
+            
+            # TOTAL LISTINGS BY COUNTRY CHOROPLETH MAP
+            country_df = df.query(query).groupby(['Country'],as_index=False)['Name'].count().rename(columns={'Name' : 'Total_Listings'}) # type: ignore
+            fig = px.choropleth(country_df,
+                                title='Total Listings in each Country',
+                                locations='Country',
+                                locationmode='country names',
+                                color='Total_Listings',
+                                color_continuous_scale=px.colors.sequential.Plasma
+                               )
+            st.plotly_chart(fig,use_container_width=True)
+        
+# EXPLORE PAGE
+if selected == "Explore":
+    st.markdown("## Explore more about the Airbnb data")
     
+    # GETTING USER INPUTS
+    country = st.sidebar.multiselect('Select a Country',sorted(df.Country.unique()),sorted(df.Country.unique()))
+    prop = st.sidebar.multiselect('Select Property_type',sorted(df.Property_type.unique()),sorted(df.Property_type.unique()))
+    room = st.sidebar.multiselect('Select Room_type',sorted(df.Room_type.unique()),sorted(df.Room_type.unique()))
+    price = st.slider('Select Price',df.Price.min(),df.Price.max(),(df.Price.min(),df.Price.max()))
+    
+    # CONVERTING THE USER INPUT INTO QUERY
+    query = f'Country in {country} & Room_type in {room} & Property_type in {prop} & Price >= {price[0]} & Price <= {price[1]}'
+    
+    # HEADING 1
+    st.markdown("## Price Analysis")
+    
+    # CREATING COLUMNS
+    col1,col2 = st.columns(2,gap='medium')
+    
+    with col1:
+        
+        # AVG PRICE BY ROOM TYPE BARCHART
+        pr_df = df.query(query).groupby('Room_type',as_index=False)['Price'].mean().sort_values(by='Price') # type: ignore
+        fig = px.bar(data_frame=pr_df,
+                     x='Room_type',
+                     y='Price',
+                     color='Price',
+                     title='Avg Price in each Room type'
+                    )
+        st.plotly_chart(fig,use_container_width=True)
+        
+        # HEADING 2
+        st.markdown("## Availability Analysis")
+        
+        # AVAILABILITY BY ROOM TYPE BOX PLOT
+        fig = px.box(data_frame=df.query(query),
+                     x='Room_type',
+                     y='Availability_365',
+                     color='Room_type',
+                     title='Availability by Room_type'
+                    )
+        st.plotly_chart(fig,use_container_width=True)
+        
+    with col2:
+        
+        # AVG PRICE IN COUNTRIES SCATTERGEO
+        country_df = df.query(query).groupby('Country',as_index=False)['Price'].mean()
+        fig = px.scatter_geo(data_frame=country_df,
+                                       locations='Country',
+                                       color= 'Price', 
+                                       hover_data=['Price'],
+                                       locationmode='country names',
+                                       size='Price',
+                                       title= 'Avg Price in each Country',
+                                       color_continuous_scale='agsunset'
+                            )
+        col2.plotly_chart(fig,use_container_width=True)
+        
+        # BLANK SPACE
+        st.markdown("#   ")
+        st.markdown("#   ")
+        
+        # AVG AVAILABILITY IN COUNTRIES SCATTERGEO
+        country_df = df.query(query).groupby('Country',as_index=False)['Availability_365'].mean()
+        country_df.Availability_365 = country_df.Availability_365.astype(int)
+        fig = px.scatter_geo(data_frame=country_df,
+                                       locations='Country',
+                                       color= 'Availability_365', 
+                                       hover_data=['Availability_365'],
+                                       locationmode='country names',
+                                       size='Availability_365',
+                                       title= 'Avg Availability in each Country',
+                                       color_continuous_scale='agsunset'
+                            )
+        st.plotly_chart(fig,use_container_width=True)
